6770da30358e4af416bc053c794db1ccb2b04c3d
unknown <unknown>
2023-07-31 15:50:34 +0000

    [clang-format] Fix braced initializer formatting with templated base class initializer
    Automated commit created by applying diff 545689
    
    Phabricator-ID: PHID-HMBT-r43dlqer7mb3xe6itusb
    Review-ID: D156705

--- clang/unittests/Format/FormatTest.cpp
+++ clang/unittests/Format/FormatTest.cpp
@@ -13458,6 +13458,7 @@ TEST_F(FormatTest, LayoutCxx11BraceInitializers) {
   verifyFormat(
       "class A {\n"
       "  A() : a{} {}\n"
+      "  A() : Base<int>{} {}\n"
       "  A(int b) : b(b) {}\n"
       "  A(int a, int b) : a(a), bs{{bs...}} { f(); }\n"
       "  int a, b;\n"
