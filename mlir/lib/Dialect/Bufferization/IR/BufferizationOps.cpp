3f19f8c86a674560469ed1c96d115d68b1218686
Martin Erhart <merhart@google.com>
2023-08-01 16:53:24 +0000

    [mlir][bufferization] DeallocOp canonicalizer removing memrefs that are never deallocated
    Automated commit created by applying diff 546106
    
    Phabricator-ID: PHID-HMBT-3eifkk6zlu64ji32uzsk
    Review-ID: D156807

--- mlir/lib/Dialect/Bufferization/IR/BufferizationOps.cpp
+++ mlir/lib/Dialect/Bufferization/IR/BufferizationOps.cpp
@@ -866,11 +866,60 @@ struct EraseEmptyDealloc : public OpRewritePattern<DeallocOp> {
   }
 };
 
+/// Removes memrefs from the deallocation list if their associated condition is
+/// always 'false'.
+///
+/// Example:
+/// ```
+/// %0:2 = bufferization.dealloc (%arg0, %arg1 : memref<2xi32>, memref<2xi32>)
+///                           if (%arg2, %false)
+/// ```
+/// becomes
+/// ```
+/// %0 = bufferization.dealloc (%arg0 : memref<2xi32>) if (%arg2)
+/// ```
+struct EraseAlwaysFalseDealloc : public OpRewritePattern<DeallocOp> {
+  using OpRewritePattern<DeallocOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(DeallocOp deallocOp,
+                                PatternRewriter &rewriter) const override {
+    SmallVector<Value> newMemrefs, newConditions;
+    SmallVector<int32_t> replacements;
+    int i = 0;
+    for (auto [res, memref, cond] :
+         llvm::zip(deallocOp.getUpdatedConditions(), deallocOp.getMemrefs(),
+                   deallocOp.getConditions())) {
+      if (matchPattern(cond, m_Zero())) {
+        res.replaceAllUsesWith(cond);
+        replacements.push_back(-1);
+        continue;
+      }
+      newMemrefs.push_back(memref);
+      newConditions.push_back(cond);
+      replacements.push_back(i++);
+    }
+    if (newMemrefs.size() < deallocOp.getMemrefs().size()) {
+      auto newDeallocOp =
+          rewriter.create<DeallocOp>(deallocOp.getLoc(), newMemrefs,
+                                     newConditions, deallocOp.getRetained());
+      for (auto [repl, res] :
+           llvm::zip(replacements, deallocOp.getUpdatedConditions()))
+        if (repl != -1)
+          res.replaceAllUsesWith(newDeallocOp.getResult(repl));
+      deallocOp.erase();
+      return success();
+    }
+    return failure();
+  }
+};
+
 } // anonymous namespace
 
 void DeallocOp::getCanonicalizationPatterns(RewritePatternSet &results,
                                             MLIRContext *context) {
-  results.add<DeallocRemoveDuplicates, EraseEmptyDealloc>(context);
+  results
+      .add<DeallocRemoveDuplicates, EraseEmptyDealloc, EraseAlwaysFalseDealloc>(
+          context);
 }
 
 //===----------------------------------------------------------------------===//
