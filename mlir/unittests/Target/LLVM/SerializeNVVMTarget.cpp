060f5157f5aa4334d4c1bfdb3dcf3e3e415b88dc
Fabian Mora <fmora.dev@gmail.com>
2023-08-01 00:54:40 +0000

    [mlir][NVVM] Adds the NVVM target attribute.
    Automated commit created by applying diff 545884
    
    Phabricator-ID: PHID-HMBT-vjfyyl2sbrs2vjxqs6hy
    Review-ID: D154117

---
+++ mlir/unittests/Target/LLVM/SerializeNVVMTarget.cpp
@@ -0,0 +1,62 @@
+//===- SerializeNVVMTarget.cpp ----------------------------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Dialect/GPU/IR/GPUDialect.h"
+#include "mlir/Dialect/LLVMIR/NVVMDialect.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/Parser/Parser.h"
+#include "mlir/Target/LLVM/NVVM/Target.h"
+#include "mlir/Target/LLVMIR/Dialect/Builtin/BuiltinToLLVMIRTranslation.h"
+#include "mlir/Target/LLVMIR/Dialect/GPU/GPUToLLVMIRTranslation.h"
+#include "mlir/Target/LLVMIR/Dialect/LLVMIR/LLVMToLLVMIRTranslation.h"
+
+#include "llvm/IRReader/IRReader.h"
+#include "llvm/Support/MemoryBufferRef.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/TargetParser/Host.h"
+
+#include "gmock/gmock.h"
+
+using namespace mlir;
+
+TEST(MLIRTargetLLVM, SerializeNVVMModule) {
+  std::string moduleStr = R"mlir(
+  gpu.module @kernels {
+    llvm.func @kernel(%arg0: f32) attributes {gpu.kernel, nvvm.kernel} {
+      llvm.return
+    }
+  }
+  )mlir";
+
+  DialectRegistry registry;
+  registerBuiltinDialectTranslation(registry);
+  registerLLVMDialectTranslation(registry);
+  registerGPUDialectTranslation(registry);
+  registerNVVMTarget(registry);
+  MLIRContext context(registry);
+
+  OwningOpRef<ModuleOp> module =
+      parseSourceString<ModuleOp>(moduleStr, &context);
+  ASSERT_TRUE(!!module);
+
+  // Create a NVVM target.
+  NVVM::NVVMTargetAttr target = NVVM::NVVMTargetAttr::get(&context);
+
+  // Serialize the module.
+  auto serializer = dyn_cast<gpu::TargetAttrInterface>(target);
+  ASSERT_TRUE(!!serializer);
+  for (auto gpuModule : (*module).getBody()->getOps<gpu::GPUModuleOp>()) {
+    std::optional<SmallVector<char, 0>> object =
+        serializer.serializeToObject(gpuModule, {});
+    // Check that the serializer was successful.
+    ASSERT_TRUE(object != std::nullopt);
+    ASSERT_TRUE(object->size() > 0);
+  }
+}
