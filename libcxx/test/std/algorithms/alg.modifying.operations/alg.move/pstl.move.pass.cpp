e591b9d5a7bd508c1f76267b08fcc3704e050b19
Nikolas Klauser <nikolasklauser@berlin.de>
2023-08-03 16:39:43 +0000

    [libc++][PSTL] Implement std::move
    Automated commit created by applying diff 546908
    
    Phabricator-ID: PHID-HMBT-3vyy6keti5v34fotxim4
    Review-ID: D155330

--- libcxx/test/std/algorithms/alg.modifying.operations/alg.move/pstl.move.pass.cpp
+++ libcxx/test/std/algorithms/alg.modifying.operations/alg.move/pstl.move.pass.cpp
@@ -13,7 +13,7 @@
 // <algorithm>
 
 // template<class ExecutionPolicy, class ForwardIterator1, class ForwardIterator2>
-//   ForwardIterator2 copy(ExecutionPolicy&& policy,
+//   ForwardIterator2 move(ExecutionPolicy&& policy,
 //                         ForwardIterator1 first, ForwardIterator1 last,
 //                         ForwardIterator2 result);
 
@@ -24,10 +24,10 @@
 #include "test_execution_policies.h"
 #include "test_iterators.h"
 
-EXECUTION_POLICY_SFINAE_TEST(copy);
+EXECUTION_POLICY_SFINAE_TEST(move);
 
-static_assert(sfinae_test_copy<int, int*, int*, bool (*)(int)>);
-static_assert(!sfinae_test_copy<std::execution::parallel_policy, int*, int*, int>);
+static_assert(sfinae_test_move<int, int*, int*, int*>);
+static_assert(!sfinae_test_move<std::execution::parallel_policy, int*, int*, int*>);
 
 template <class Iter1, class Iter2>
 struct TestInt {
@@ -41,7 +41,7 @@ struct TestInt {
 
       std::vector<int> out(std::size(a));
       decltype(auto) ret =
-          std::copy(policy, Iter1(std::data(a)), Iter1(std::data(a) + std::size(a)), Iter2(std::data(out)));
+          std::move(policy, Iter1(std::data(a)), Iter1(std::data(a) + std::size(a)), Iter2(std::data(out)));
       static_assert(std::is_same_v<decltype(ret), Iter2>);
       assert(base(ret) == std::data(out) + std::size(out));
       for (int i = 0; i != size; ++i)
@@ -50,16 +50,16 @@ struct TestInt {
   }
 };
 
-struct CopiedToTester {
-  bool copied_to   = false;
-  CopiedToTester() = default;
-  CopiedToTester(const CopiedToTester&) {}
-  CopiedToTester& operator=(const CopiedToTester&) {
-    assert(!copied_to);
-    copied_to = true;
+struct MovedToTester {
+  bool moved_to   = false;
+  MovedToTester() = default;
+  MovedToTester(MovedToTester&&) {}
+  MovedToTester& operator=(MovedToTester&&) {
+    assert(!moved_to);
+    moved_to = true;
     return *this;
   }
-  ~CopiedToTester() = default;
+  ~MovedToTester() = default;
 };
 
 template <class Iter1, class Iter2>
@@ -68,13 +68,13 @@ struct TestNonTrivial {
   void operator()(Policy&& policy) {
     // simple test
     for (const int size : {0, 1, 2, 100, 350}) {
-      std::vector<CopiedToTester> a(size);
+      std::vector<MovedToTester> a(size);
 
-      std::vector<CopiedToTester> out(std::size(a));
-      auto ret = std::copy(policy, Iter1(std::data(a)), Iter1(std::data(a) + std::size(a)), Iter2(std::data(out)));
+      std::vector<MovedToTester> out(std::size(a));
+      auto ret = std::move(policy, Iter1(std::data(a)), Iter1(std::data(a) + std::size(a)), Iter2(std::data(out)));
       assert(base(ret) == std::data(out) + std::size(out));
-      assert(std::all_of(std::begin(out), std::end(out), [](CopiedToTester& t) { return t.copied_to; }));
-      assert(std::none_of(std::begin(a), std::end(a), [](CopiedToTester& t) { return t.copied_to; }));
+      assert(std::all_of(std::begin(out), std::end(out), [](MovedToTester& t) { return t.moved_to; }));
+      assert(std::none_of(std::begin(a), std::end(a), [](MovedToTester& t) { return t.moved_to; }));
     }
   }
 };
@@ -93,10 +93,10 @@ int main(int, char**) {
                   }});
 
   types::for_each(
-      types::forward_iterator_list<CopiedToTester*>{}, types::apply_type_identity{[](auto v) {
+      types::forward_iterator_list<MovedToTester*>{}, types::apply_type_identity{[](auto v) {
         using Iter = typename decltype(v)::type;
         types::for_each(
-            types::forward_iterator_list<CopiedToTester*>{},
+            types::forward_iterator_list<MovedToTester*>{},
             TestIteratorWithPolicies< types::partial_instantiation<TestNonTrivial, Iter>::template apply>{});
       }});
 
