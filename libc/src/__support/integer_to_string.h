ac7156e8ac0052abc54491829d0aedb9e07d393a
Guillaume Chatelet <gchatelet@google.com>
2023-08-03 09:55:54 +0000

    [libc] Better IntegerToString API
    Automated commit created by applying diff 546779
    
    Phabricator-ID: PHID-HMBT-xukuhhsyfc2ibuxraz6h
    Review-ID: D156981

--- libc/src/__support/integer_to_string.h
+++ libc/src/__support/integer_to_string.h
@@ -5,12 +5,58 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+//
+// Converts an integer to a string.
+//
+// By default, the string is written to an internal buffer and accessed via the
+// 'string_view' operator.
+//
+//   const IntegerTo<DecString, int> buffer(42);
+//   const cpp::string_view view(buffer);
+//
+// The buffer is allocated on the stack and its size is so that the conversion
+// always succeeds.
+//
+// It is also possible to write the data to a preallocated buffer, but in this
+// case the operation may fail.
+//
+//   char buffer[8];
+//   if( auto maybe_view = IntegerTo<DecString, int>::write(42, buffer)) {
+//     const cpp::string_view view = *maybe_view;
+//   }
+//
+// The first template parameter defines how the integer is formatted.
+// Available default are 'BinString', 'OctString', 'DecString' and 'HexString'.
+//
+// For 'BinString', 'OctString' and 'HexString' the value is always interpreted
+// as a positive type but 'DecString' will honor negative values. e.g.,
+//
+//   IntegerTo<BinString, int8_t>(-1) // "11111111"
+//   IntegerTo<OctString, int8_t>(-1) // "377"
+//   IntegerTo<DecString, int8_t>(-1) // "-1"
+//   IntegerTo<HexString, int8_t>(-1) // "ff"
+//
+// Additionnally, the default can be changed by navigating the subtypes:
+//  - WithPrefix    : Adds "0b", "0", "0x" for binary, octal and hexadecimal
+//  - WithWidth<XX> : Pad string to XX characters filling leading digits with 0
+//  - Uppercase     : Use uppercase letters (only for HexString)
+//  - WithSign      : Prepend '+' for positive values (only for DecString)
+//
+// Examples
+// --------
+//   IntegerTo<DecString::WithWidth<2>::WithSign, int8_t>(0)     // "+00"
+//   IntegerTo<DecString::WithWidth<2>::WithSign, int8_t>(-1)    // "-01"
+//   IntegerTo<HexString::WithPrefix::Uppercase, uint8_t>(255)   // "0xFF"
+//   IntegerTo<HexString::WithWidth<8>::Uppercase, uint8_t>(255) // "000000FF"
+//===----------------------------------------------------------------------===//
 
 #ifndef LLVM_LIBC_SRC_SUPPORT_INTEGER_TO_STRING_H
 #define LLVM_LIBC_SRC_SUPPORT_INTEGER_TO_STRING_H
 
 #include <stdint.h>
 
+#include "src/__support/CPP/array.h"
+#include "src/__support/CPP/bit.h"
 #include "src/__support/CPP/optional.h"
 #include "src/__support/CPP/span.h"
 #include "src/__support/CPP/string_view.h"
@@ -19,78 +65,38 @@
 
 namespace __llvm_libc {
 
-// Convert integer values to their string representation.
-//
-// Example usage:
-//   int a = 1234567;
-//
-//   // Convert to hexadecimal string:
-//   char hexbuf[IntegerToString::hex_bufsize<int>()];
-//   auto str = IntegerToString::hex(
-//       a, hexbuf, false /* generate upper case characters */);
-//
-//   // Convert to decimal string:
-//   char decbuf[IntegerToString::dec_bufsize<int>()];
-//   auto str = IntegerToString::dec(a, decbuf);
-//
-//   // Convert to octal string:
-//   char octbuf[IntegerToString::oct_bufsize<int>(a)];
-//   auto str = IntegerToString::dec(a, octbuf);
-//
-//   // Convert to binary string:
-//   char binbuf[IntegerToString::bin_bufsize<int>(a)];
-//   auto str = IntegerToString::bin(a, binbuf);
-//
-//   // Convert to base 30 string:
-//   char b30buf[IntegerToString::bufsize<30, int>(a)];
-//   auto str = IntegerToString::convert<30>(a, b30buf);
-class IntegerToString {
-  LIBC_INLINE static cpp::string_view convert_uintmax(uintmax_t uval,
-                                                      cpp::span<char> &buffer,
-                                                      bool lowercase,
-                                                      const uint8_t conv_base) {
-    const char a = lowercase ? 'a' : 'A';
+namespace details {
 
-    size_t len = 0;
+template <uint8_t base, bool prefix = false, bool is_uppercase = false,
+          size_t min_digits = 1, bool force_sign = false>
+struct Config {
+  static constexpr size_t MIN_DIGITS = min_digits;
+  static constexpr uint8_t BASE = base;
+  static constexpr bool IS_UPPERCASE = is_uppercase;
+  static constexpr bool PREFIX = prefix;
+  static constexpr char FORCE_SIGN = force_sign;
 
-    size_t buffptr = buffer.size();
-    if (uval == 0) {
-      buffer[buffptr - 1] = '0';
-      --buffptr;
-    } else {
-      for (; uval > 0; --buffptr, uval /= conv_base) {
-        uintmax_t digit = (uval % conv_base);
-        buffer[buffptr - 1] = static_cast<char>(digit < 10 ? digit + '0' : digit + a - 10);
-      }
-    }
-    len = buffer.size() - buffptr;
+  template <size_t value>
+  using WithWidth = Config<BASE, PREFIX, IS_UPPERCASE, value, FORCE_SIGN>;
+  using WithPrefix = Config<BASE, true, IS_UPPERCASE, MIN_DIGITS, FORCE_SIGN>;
+  using Uppercase = Config<BASE, PREFIX, true, MIN_DIGITS, FORCE_SIGN>;
+  using WithSign = Config<BASE, PREFIX, IS_UPPERCASE, MIN_DIGITS, true>;
 
-    return cpp::string_view(buffer.data() + buffer.size() - len, len);
-  }
+  // Invariants
+  static constexpr uint8_t NUMERICAL_DIGITS = 10;
+  static constexpr uint8_t ALPHA_DIGITS = 26;
+  static constexpr uint8_t MAX_DIGIT = NUMERICAL_DIGITS + ALPHA_DIGITS;
+  static_assert(BASE > 1 && BASE <= MAX_DIGIT);
+};
 
-  LIBC_INLINE static cpp::string_view convert_intmax(intmax_t val,
-                                                     cpp::span<char> &buffer,
-                                                     bool lowercase,
-                                                     const uint8_t conv_base) {
-    if (val >= 0)
-      return convert_uintmax(uintmax_t(val), buffer, lowercase, conv_base);
-    uintmax_t uval = uintmax_t(-val);
-    auto str_view = convert_uintmax(uval, buffer, lowercase, conv_base);
-    size_t len = str_view.size();
-    ++len;
-    buffer[buffer.size() - len] = '-';
-    return cpp::string_view(buffer.data() + buffer.size() - len, len);
-  }
+} // namespace details
 
-  LIBC_INLINE static constexpr size_t floor_log_2(size_t num) {
-    size_t i = 0;
-    for (; num > 1; num /= 2) {
-      ++i;
-    }
-    return i;
-  }
+using BinString = details::Config<2>;
+using OctString = details::Config<8>;
+using DecString = details::Config<10>;
+using HexString = details::Config<16>;
 
-public:
+template <typename Conf, typename T> class IntegerTo {
   // We size the string buffer for base 10 using an approximation algorithm:
   //
   //   size = ceil(sizeof(T) * 5 / 2)
@@ -112,93 +118,136 @@ public:
   // For other bases, we approximate by rounding down to the nearest power of
   // two base, since the space needed is easy to calculate and it won't
   // overestimate by too much.
-  template <uint8_t BASE, typename T>
-  LIBC_INLINE static constexpr size_t bufsize() {
-    constexpr size_t BITS_PER_DIGIT = floor_log_2(BASE);
-    constexpr size_t BUFSIZE_COMMON =
-        ((sizeof(T) * 8 + (BITS_PER_DIGIT - 1)) / BITS_PER_DIGIT);
-    constexpr size_t BUFSIZE_BASE10 = (sizeof(T) * 5 + 1) / 2;
-    return (cpp::is_signed<T>() ? 1 : 0) +
-           (BASE == 10 ? BUFSIZE_BASE10 : BUFSIZE_COMMON);
+  LIBC_INLINE static constexpr size_t compute_digit_size() {
+    if constexpr (Conf::BASE == 10)
+      return (sizeof(T) * 5 + 1) / 2;
+    auto floor_log_2 = [](size_t num) -> size_t {
+      size_t i = 0;
+      for (; num > 1; num /= 2)
+        ++i;
+      return i;
+    };
+    constexpr size_t BITS_PER_DIGIT = floor_log_2(Conf::BASE);
+    return ((sizeof(T) * 8 + (BITS_PER_DIGIT - 1)) / BITS_PER_DIGIT);
   }
-
-  template <typename T> LIBC_INLINE static constexpr size_t dec_bufsize() {
-    return bufsize<10, T>();
+  LIBC_INLINE static constexpr size_t max(size_t a, size_t b) {
+    return a < b ? b : a;
   }
-
-  template <typename T> LIBC_INLINE static constexpr size_t hex_bufsize() {
-    return bufsize<16, T>();
+  LIBC_INLINE static char digit_char(uint8_t digit) {
+    if (digit < 10)
+      return '0' + digit;
+    return (Conf::IS_UPPERCASE ? 'A' : 'a') + (digit - 10);
   }
+  // constants
+  using UNSIGNED_T = cpp::make_unsigned_t<T>;
+  static constexpr size_t BUFFER_SIZE =
+      max(compute_digit_size(), Conf::MIN_DIGITS) + (Conf::BASE == 10 ? 1 : 0) +
+      (Conf::PREFIX ? 2 : 0);
+  // Invariants
+  static_assert(cpp::is_integral_v<T>);
+  static_assert(BUFFER_SIZE > 1);
 
-  template <typename T> LIBC_INLINE static constexpr size_t oct_bufsize() {
-    return bufsize<8, T>();
-  }
+  // Internal abstraction to handle different kind of buffers.
+  struct Sink {
+    char *const begin;
+    char *const end;
+    char *ptr;
 
-  template <typename T> LIBC_INLINE static constexpr size_t bin_bufsize() {
-    return bufsize<2, T>();
-  }
+    explicit Sink(cpp::span<char> buffer)
+        : begin(buffer.begin()), end(buffer.end()), ptr(end) {}
 
-  template <uint8_t BASE, typename T,
-            cpp::enable_if_t<2 <= BASE && BASE <= 36 && cpp::is_integral_v<T>,
-                             int> = 0>
-  LIBC_INLINE static cpp::optional<cpp::string_view>
-  convert(T val, cpp::span<char> buffer, bool lowercase = true) {
-    if (buffer.size() < bufsize<BASE, T>())
-      return cpp::optional<cpp::string_view>();
-    if (cpp::is_signed_v<T>)
-      return convert_intmax(intmax_t(val), buffer, lowercase, BASE);
-    else
-      return convert_uintmax(uintmax_t(val), buffer, lowercase, BASE);
-  }
+    char last_char() const { return *ptr; }
+    size_t size() const { return end - ptr; }
+    size_t empty() const { return size() == 0; }
 
-  template <typename T, cpp::enable_if_t<cpp::is_integral_v<T>, int> = 0>
-  LIBC_INLINE static cpp::optional<cpp::string_view>
-  dec(T val, cpp::span<char> buffer) {
-    return convert<10>(val, buffer);
-  }
-
-  template <typename T, cpp::enable_if_t<cpp::is_integral_v<T> &&
-                                             (sizeof(T) <= sizeof(uintmax_t)),
-                                         int> = 0>
-  LIBC_INLINE static cpp::optional<cpp::string_view>
-  hex(T val, cpp::span<char> buffer, bool lowercase = true) {
-    return convert<16>(val, buffer, lowercase);
-  }
-
-  template <typename T, cpp::enable_if_t<cpp::is_integral_v<T> &&
-                                             (sizeof(T) > sizeof(uintmax_t)) &&
-                                             sizeof(T) % sizeof(uintmax_t) == 0,
-                                         int> = 0>
-  LIBC_INLINE static cpp::optional<cpp::string_view>
-  hex(T val, cpp::span<char> buffer, bool lowercase = true) {
-    // We will assume the buffer is exactly sized, which will be the case if
-    // it was sized using the bufsize method.
-    constexpr size_t BLOCKS = sizeof(T) / sizeof(uintmax_t);
-    constexpr size_t UINTMAX_BUFSIZE = bufsize<16, uintmax_t>();
-    // We will zero out the buffer. This specialization is not used to
-    // implement a public function so zeroing out byte-by-byte does not
-    // have any affect on runtime or user expectations.
-    for (size_t i = 0; i < buffer.size(); ++i)
-      buffer[i] = '0';
-    for (size_t i = 0; i < BLOCKS; ++i, val >>= (sizeof(uintmax_t) * 8)) {
-      uintmax_t block_val = static_cast<uintmax_t>(val);
-      hex(block_val,
-          buffer.subspan((BLOCKS - i - 1) * UINTMAX_BUFSIZE, UINTMAX_BUFSIZE),
-          lowercase);
+    void push_front(char c) {
+      if (buffer_overrun())
+        return;
+      --ptr;
+      *ptr = c;
     }
-    return cpp::string_view(buffer.data(), buffer.size());
+
+    // Returns whether the sink is still in a valid state.
+    operator bool() const { return !buffer_overrun(); }
+
+  private:
+    bool buffer_overrun() const { return ptr < begin; }
+  };
+
+  // Internal buffer.
+  struct Buffer {
+    cpp::array<char, BUFFER_SIZE> array;
+    size_t size = 0;
+    cpp::span<char> as_span() { return cpp::span<char>(array); }
+  };
+
+  LIBC_INLINE static void convert(UNSIGNED_T value, Sink &sink) {
+    for (; sink && value != 0; value /= Conf::BASE) {
+      const uint8_t digit(value % Conf::BASE);
+      sink.push_front(digit_char(digit));
+    }
   }
 
-  template <typename T, cpp::enable_if_t<cpp::is_integral_v<T>, int> = 0>
-  LIBC_INLINE static cpp::optional<cpp::string_view>
-  oct(T val, cpp::span<char> buffer) {
-    return convert<8>(val, buffer);
+  LIBC_INLINE static void process(T value, Sink &sink) {
+    bool is_negative = false;
+    if constexpr (Conf::BASE == 10) {
+      is_negative = value < 0;
+      convert(is_negative ? -value : value, sink);
+    } else {
+      convert(cpp::bit_cast<UNSIGNED_T>(value), sink);
+    }
+    // width
+    while (sink && sink.size() < Conf::MIN_DIGITS)
+      sink.push_front('0');
+    // sign
+    if constexpr (Conf::BASE == 10) {
+      if (is_negative)
+        sink.push_front('-');
+      if (!is_negative && Conf::FORCE_SIGN)
+        sink.push_front('+');
+    }
+    // prefix
+    if (Conf::PREFIX) {
+      if (Conf::BASE == 2) {
+        sink.push_front('b');
+        sink.push_front('0');
+      }
+      if (Conf::BASE == 16) {
+        sink.push_front('x');
+        sink.push_front('0');
+      }
+      if (Conf::BASE == 8 && (sink.empty() || sink.last_char() != '0')) {
+        sink.push_front('0');
+      }
+    }
   }
 
-  template <typename T, cpp::enable_if_t<cpp::is_integral_v<T>, int> = 0>
+  Buffer buffer;
+
+public:
+  explicit IntegerTo(T value) {
+    Sink sink(buffer.as_span());
+    process(value, sink);
+    buffer.size = sink.size();
+  }
+
+  size_t size() const { return buffer.size; }
+
+  // Prevent returning dangling string_view.
+  explicit operator cpp::string_view() && = delete;
+  explicit operator cpp::string_view() const & {
+    return {buffer.array.end() - size(), size()};
+  }
+
+  LIBC_INLINE static constexpr size_t buffer_size() { return BUFFER_SIZE; }
+
   LIBC_INLINE static cpp::optional<cpp::string_view>
-  bin(T val, cpp::span<char> buffer) {
-    return convert<2>(val, buffer);
+  write_to(cpp::span<char> span, T value) {
+    Sink sink(span);
+    process(value, sink);
+    if (sink)
+      return cpp::string_view(span.end() - sink.size(), sink.size());
+    return cpp::nullopt;
   }
 };
 
