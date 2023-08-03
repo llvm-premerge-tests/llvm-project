7a04d659d21be67379104763ee460940b006befd
Chia-hung Duan <chiahungduan@google.com>
2023-08-03 00:03:19 +0000

    [scudo] Fine tune busy-waiting in HybridMutex
    Automated commit created by applying diff 546647
    
    Phabricator-ID: PHID-HMBT-un5va6ih4vrwwtpjiqtx
    Review-ID: D156951

--- compiler-rt/lib/scudo/standalone/mutex.h
+++ compiler-rt/lib/scudo/standalone/mutex.h
@@ -35,7 +35,7 @@ public:
 #pragma nounroll
 #endif
     for (u8 I = 0U; I < NumberOfTries; I++) {
-      yieldProcessor(NumberOfYields);
+      yieldProcessor();
       if (tryLock())
         return;
     }
@@ -53,10 +53,20 @@ public:
   }
 
 private:
+  void yieldProcessor() {
+    // The value comes from the minimum time spent among operations that guarded
+    // by mutex.
+    constexpr u32 SpinTimes = 256;
+    volatile u32 V = 0;
+    for (u32 I = 0; I < SpinTimes; ++I)
+      ++V;
+  }
+
   void assertHeldImpl();
 
-  static constexpr u8 NumberOfTries = 8U;
-  static constexpr u8 NumberOfYields = 8U;
+  // TODO(chiahungduan): Adapt this value based on scenarios. E.g., primary and
+  // secondary allocator have different allocation time.
+  static constexpr u8 NumberOfTries = 32U;
 
 #if SCUDO_LINUX
   atomic_u32 M = {};
