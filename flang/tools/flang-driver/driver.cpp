80b659202ecb483cd200f3b02c886cea003f3c55
Victor Kingi <victor.kingi@arm.com>
2023-08-03 11:24:41 +0000

    [FLang] Add support for Rpass flag
    Automated commit created by applying diff 546798
    
    Phabricator-ID: PHID-HMBT-xim3cowtkae6cb23wrxr
    Review-ID: D156320

--- flang/tools/flang-driver/driver.cpp
+++ flang/tools/flang-driver/driver.cpp
@@ -30,6 +30,7 @@
 #include "llvm/Support/VirtualFileSystem.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/TargetParser/Host.h"
+#include <clang/Basic/DiagnosticFrontend.h>
 #include <stdlib.h>
 
 using llvm::StringRef;
