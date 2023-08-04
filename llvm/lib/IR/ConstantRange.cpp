63823b1f354d7560924c7cfef68a51d83e9573a9
unknown <unknown>
2023-08-04 02:40:36 +0000

    [ConstantRange] Calculate precise range for shl by -1
    Automated commit created by applying diff 547085
    
    Phabricator-ID: PHID-HMBT-3ntdgno5spy6njj5ehx7
    Review-ID: D156845

--- llvm/lib/IR/ConstantRange.cpp
+++ llvm/lib/IR/ConstantRange.cpp
@@ -1478,6 +1478,13 @@ ConstantRange::shl(const ConstantRange &Other) const {
 
   APInt OtherMax = Other.getUnsignedMax();
 
+  bool Neg = false;
+  if (getSingleElement() && isAllNegative()) {
+    Neg = true;
+    Min = Min.abs();
+    Max = Max.abs();
+  }
+
   // There's overflow!
   if (OtherMax.ugt(Max.countl_zero()))
     return getFull();
@@ -1487,6 +1494,11 @@ ConstantRange::shl(const ConstantRange &Other) const {
   Min <<= Other.getUnsignedMin();
   Max <<= OtherMax;
 
+  if (Neg) {
+    Min = -Min;
+    Max = -Max;
+    return ConstantRange::getNonEmpty(std::move(Max), std::move(Min) + 1);
+  }
   return ConstantRange::getNonEmpty(std::move(Min), std::move(Max) + 1);
 }
 
