7b59502fb82f2d9ad0df9f6177ab6b8a34ecc079
Piotr Zegar <me@piotrzegar.pl>
2023-08-05 12:25:50 +0000

    [clang-tidy] Fixed false-negative in readability-identifier-naming
    Automated commit created by applying diff 547475
    
    Phabricator-ID: PHID-HMBT-2ys7umyrln3kmjshzznn
    Review-ID: D157190

--- clang-tools-extra/test/clang-tidy/checkers/readability/identifier-naming.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/readability/identifier-naming.cpp
@@ -707,3 +707,13 @@ public:
 task ImplicitDeclTest(async_obj &a_object) {
   co_await a_object;  // CHECK-MESSAGES-NOT: warning: invalid case style for local variable
 }
+
+// Test scenario when canonical declaration will be a forward declaration
+struct class_with_forward_decl;
+// CHECK-MESSAGES: :[[@LINE-1]]:8: warning: invalid case style for class 'class_with_forward_decl' [readability-identifier-naming]
+// CHECK-FIXES: {{^}}struct CClassWithForwardDecl;
+// CHECK-FIXES: {{^}}class CClassWithForwardDecl {
+class class_with_forward_decl {
+  int __value;
+};
+
