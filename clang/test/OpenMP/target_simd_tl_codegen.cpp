91655a44e2f8eacb34dd1c45dac2baf4b8890cf8
unknown <unknown>
2023-08-08 09:42:55 +0000

    [OpenMP] Codegen support for thread_limit on target directive
    Automated commit created by applying diff 548082
    
    Phabricator-ID: PHID-HMBT-nzqv4pjy4juvvou7hunz
    Review-ID: D152054

---
+++ clang/test/OpenMP/target_simd_tl_codegen.cpp
@@ -0,0 +1,20 @@
+// This file is to test thread_limit clause on target simd directive
+
+// RUN: %clang_cc1 -fopenmp -fopenmp-version=51 -emit-llvm %s -o - | FileCheck --check-prefix=OMP51 %s
+
+// expected-no-diagnostics
+
+int thread_limit_target_simd() {
+
+// Check that the offloading function is called after setting thread_limit in the task entry function
+#pragma omp target simd thread_limit(2)
+    for(int i=0; i<2; i++) {}
+
+// OMP51: define {{.*}}thread_limit_target_simd
+// OMP51: call i32 [[OMP_TASK_ENTRY:@.+]](i32 {{.*}}%0, ptr %1)
+
+// OMP51: define internal {{.*}}i32 [[OMP_TASK_ENTRY]](i32 {{.*}}%0, ptr noalias noundef %1)
+// OMP51: call void @__kmpc_set_thread_limit(ptr @{{.+}}, i32 %{{.+}}, i32 2)
+// OMP51: call void {{.*omp_offloading.*thread_limit_target_simd.*}}
+  return 0;
+}
