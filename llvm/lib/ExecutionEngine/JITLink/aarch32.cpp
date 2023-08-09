2287b16cb81ec6b65e19a2db93777765e4300a62
Eymen Ünay <20779470+Eymay@users.noreply.github.com>
2023-08-09 19:05:53 +0000

    [JITLink][AArch32] Fixes for initial AArc32 backend
    Automated commit created by applying diff 548726
    
    Phabricator-ID: PHID-HMBT-dhnkzngk2hzjwval7ghz
    Review-ID: D157540

--- llvm/lib/ExecutionEngine/JITLink/aarch32.cpp
+++ llvm/lib/ExecutionEngine/JITLink/aarch32.cpp
@@ -175,7 +175,7 @@ bool checkRegister(const ThumbRelocation &R, HalfWords Reg) {
 template <EdgeKind_aarch32 Kind>
 void writeRegister(WritableThumbRelocation &R, HalfWords Reg) {
   static constexpr HalfWords Mask = FixupInfo<Kind>::RegMask;
-  assert((Mask.Hi & Reg.Hi) == Reg.Hi && (Mask.Hi & Reg.Hi) == Reg.Hi &&
+  assert((Mask.Hi & Reg.Hi) == Reg.Hi && (Mask.Lo & Reg.Lo) == Reg.Lo &&
          "Value bits exceed bit range of given mask");
   R.Hi = (R.Hi & ~Mask.Hi) | Reg.Hi;
   R.Lo = (R.Lo & ~Mask.Lo) | Reg.Lo;
@@ -184,7 +184,7 @@ void writeRegister(WritableThumbRelocation &R, HalfWords Reg) {
 template <EdgeKind_aarch32 Kind>
 void writeImmediate(WritableThumbRelocation &R, HalfWords Imm) {
   static constexpr HalfWords Mask = FixupInfo<Kind>::ImmMask;
-  assert((Mask.Hi & Imm.Hi) == Imm.Hi && (Mask.Hi & Imm.Hi) == Imm.Hi &&
+  assert((Mask.Hi & Imm.Hi) == Imm.Hi && (Mask.Lo & Imm.Lo) == Imm.Lo &&
          "Value bits exceed bit range of given mask");
   R.Hi = (R.Hi & ~Mask.Hi) | Imm.Hi;
   R.Lo = (R.Lo & ~Mask.Lo) | Imm.Lo;
@@ -242,7 +242,7 @@ Expected<int64_t> readAddendThumb(LinkGraph &G, Block &B, const Edge &E,
   case Thumb_Jump24:
     if (!checkOpcode<Thumb_Jump24>(R))
       return makeUnexpectedOpcodeError(G, R, Kind);
-    if (R.Lo & FixupInfo<Thumb_Jump24>::LoBitConditional)
+    if (~R.Lo & FixupInfo<Thumb_Jump24>::LoBitConditional)
       return make_error<JITLinkError>("Relocation expects an unconditional "
                                       "B.W branch instruction: " +
                                       StringRef(G.getEdgeKindName(Kind)));
@@ -352,7 +352,7 @@ Error applyFixupThumb(LinkGraph &G, Block &B, const Edge &E,
   case Thumb_Jump24: {
     if (!checkOpcode<Thumb_Jump24>(R))
       return makeUnexpectedOpcodeError(G, R, Kind);
-    if (R.Lo & FixupInfo<Thumb_Jump24>::LoBitConditional)
+    if (~R.Lo & FixupInfo<Thumb_Jump24>::LoBitConditional)
       return make_error<JITLinkError>("Relocation expects an unconditional "
                                       "B.W branch instruction: " +
                                       StringRef(G.getEdgeKindName(Kind)));
@@ -471,6 +471,7 @@ const char *getEdgeKindName(Edge::Kind K) {
 
   switch (K) {
     KIND_NAME_CASE(Data_Delta32)
+    KIND_NAME_CASE(Data_Pointer32)
     KIND_NAME_CASE(Arm_Call)
     KIND_NAME_CASE(Thumb_Call)
     KIND_NAME_CASE(Thumb_Jump24)
