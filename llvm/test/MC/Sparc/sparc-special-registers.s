97110aeeabad152e626ba8522ba180486e470d86
unknown <unknown>
2023-08-13 13:11:18 +0000

    [SPARC][IAS] Add definitions for v9 State Registers
    Automated commit created by applying diff 549695
    
    Phabricator-ID: PHID-HMBT-u5oju6yvnx6atmytxf6s
    Review-ID: D157231

--- llvm/test/MC/Sparc/sparc-special-registers.s
+++ llvm/test/MC/Sparc/sparc-special-registers.s
@@ -1,4 +1,4 @@
-! RUN: llvm-mc %s -arch=sparc -show-encoding | FileCheck %s
+! RUN: llvm-mc %s -arch=sparc   -show-encoding | FileCheck %s
 ! RUN: llvm-mc %s -arch=sparcv9 -show-encoding | FileCheck %s
 
         ! CHECK: rd %y, %i0            ! encoding: [0xb1,0x40,0x00,0x00]
@@ -34,12 +34,6 @@
         ! CHECK: wr %i0, 5, %tbr          ! encoding: [0x81,0x9e,0x20,0x05]
         wr %i0, 5, %tbr
 
-        ! CHECK: rd %asr6, %i0         ! encoding: [0xb1,0x41,0x80,0x00]
-        rd %fprs, %i0
-
-        ! CHECK: wr %i0, 7, %asr6      ! encoding: [0x8d,0x86,0x20,0x07]
-        wr %i0, 7, %fprs
-
         ! CHECK: ld [%g2+20], %fsr     ! encoding: [0xc1,0x08,0xa0,0x14]
         ld [%g2 + 20],%fsr
 
@@ -54,3 +48,16 @@
 
         ! CHECK: std %fq, [%g6+%i2]    ! encoding: [0xc1,0x31,0x80,0x1a]
         std %fq, [%g6 + %i2]
+
+! RUN: llvm-mc %s -arch=sparc   -show-encoding | FileCheck %s --check-prefix=V8
+! RUN: llvm-mc %s -arch=sparcv9 -show-encoding | FileCheck %s --check-prefix=V9
+
+!! Those instructions are processed differently on V8 and V9
+
+! V8: rd %asr6, %i0         ! encoding: [0xb1,0x41,0x80,0x00]
+! V9: rd %fprs, %i0         ! encoding: [0xb1,0x41,0x80,0x00]
+rd %fprs, %i0
+
+! V8: wr %i0, 7, %asr6      ! encoding: [0x8d,0x86,0x20,0x07]
+! V9: wr %i0, 7, %fprs      ! encoding: [0x8d,0x86,0x20,0x07]
+wr %i0, 7, %fprs
