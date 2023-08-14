c0be0a72f2a19c54d19fb04ebfc28183aee50d65
Craig Topper <craig.topper@sifive.com>
2023-08-14 17:40:58 +0000

    [RISCV][GISel] Make s32 a legal type for RV64 for any operation that has a W version.
    Automated commit created by applying diff 550022
    
    Phabricator-ID: PHID-HMBT-qmku4evv3f5yk6tj4lfs
    Review-ID: D157770

--- llvm/lib/Target/RISCV/GISel/RISCVLegalizerInfo.cpp
+++ llvm/lib/Target/RISCV/GISel/RISCVLegalizerInfo.cpp
@@ -21,15 +21,21 @@ using namespace llvm;
 
 RISCVLegalizerInfo::RISCVLegalizerInfo(const RISCVSubtarget &ST) {
   const unsigned XLen = ST.getXLen();
+  const LLT s32 = LLT::scalar(32);
   const LLT XLenLLT = LLT::scalar(XLen);
   const LLT p0 = LLT::pointer(0, XLen);
 
   using namespace TargetOpcode;
 
-  getActionDefinitionsBuilder({G_ADD, G_SUB, G_AND, G_OR, G_XOR})
+  getActionDefinitionsBuilder({G_AND, G_OR, G_XOR})
       .legalFor({XLenLLT})
       .widenScalarToNextPow2(0)
       .clampScalar(0, XLenLLT, XLenLLT);
+
+  getActionDefinitionsBuilder({G_ADD, G_SUB})
+      .legalFor({s32, XLenLLT})
+      .widenScalarToNextPow2(0)
+      .clampScalar(0, s32, XLenLLT);
 
   getActionDefinitionsBuilder(
       {G_UADDE, G_UADDO, G_USUBE, G_USUBO, G_SADDE, G_SADDO, G_SSUBE, G_SSUBO})
@@ -39,7 +45,7 @@ RISCVLegalizerInfo::RISCVLegalizerInfo(const RISCVSubtarget &ST) {
       .widenScalarToNextPow2(0);
 
   getActionDefinitionsBuilder({G_ASHR, G_LSHR, G_SHL})
-      .legalFor({{XLenLLT, XLenLLT}})
+      .legalFor({{s32, s32}, {s32, XLenLLT}, {XLenLLT, XLenLLT}})
       .widenScalarToNextPow2(0)
       .clampScalar(1, XLenLLT, XLenLLT)
       .clampScalar(0, XLenLLT, XLenLLT);
@@ -82,9 +88,9 @@ RISCVLegalizerInfo::RISCVLegalizerInfo(const RISCVSubtarget &ST) {
   }
 
   getActionDefinitionsBuilder({G_CONSTANT, G_IMPLICIT_DEF})
-      .legalFor({XLenLLT, p0})
+      .legalFor({s32, XLenLLT, p0})
       .widenScalarToNextPow2(0)
-      .clampScalar(0, XLenLLT, XLenLLT);
+      .clampScalar(0, s32, XLenLLT);
 
   getActionDefinitionsBuilder(G_ICMP)
       .legalFor({{XLenLLT, XLenLLT}})
