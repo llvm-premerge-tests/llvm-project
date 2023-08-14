4f068a5242ad309db1a385a4765ae379fa40416b
Evgenii Kudriashov <evgenii.kudriashov@intel.com>
2023-08-14 22:27:47 +0000

    [X86][AMX] Fix virtual register traversing in case of GlobalIsel
    Automated commit created by applying diff 550108
    
    Phabricator-ID: PHID-HMBT-pyx6vfm5a3omu4kbjgat
    Review-ID: D157458

--- llvm/lib/Target/X86/X86TargetMachine.cpp
+++ llvm/lib/Target/X86/X86TargetMachine.cpp
@@ -532,10 +532,10 @@ void X86PassConfig::addPreRegAlloc() {
   addPass(createX86FlagsCopyLoweringPass());
   addPass(createX86DynAllocaExpander());
 
-  if (getOptLevel() != CodeGenOpt::None)
-    addPass(createX86PreTileConfigPass());
-  else
+  if (TM->Options.EnableFastISel)
     addPass(createX86FastPreTileConfigPass());
+  else
+    addPass(createX86PreTileConfigPass());
 }
 
 void X86PassConfig::addMachineSSAOptimization() {
