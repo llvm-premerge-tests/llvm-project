01307ea0afed970eae6f0f9f092cdc7ac3cdfa70
Arthur Eubanks <aeubanks@google.com>
2023-08-14 18:29:34 +0000

    [NFC] Refactor X86TargetLowering::getGlobalWrapperKind()
    Automated commit created by applying diff 550036
    
    Phabricator-ID: PHID-HMBT-azzpdhrgdb5uzkuqlmla
    Review-ID: D157907

--- llvm/lib/Target/X86/X86Subtarget.cpp
+++ llvm/lib/Target/X86/X86Subtarget.cpp
@@ -323,7 +323,7 @@ X86Subtarget::X86Subtarget(const Triple &TT, StringRef CPU, StringRef TuneCPU,
       InstrInfo(initializeSubtargetDependencies(CPU, TuneCPU, FS)),
       TLInfo(TM, *this), FrameLowering(*this, getStackAlignment()) {
   // Determine the PICStyle based on the target selected.
-  if (!isPositionIndependent())
+  if (!isPositionIndependent() || TM.getCodeModel() == CodeModel::Large)
     setPICStyle(PICStyles::Style::None);
   else if (is64Bit())
     setPICStyle(PICStyles::Style::RIPRel);
