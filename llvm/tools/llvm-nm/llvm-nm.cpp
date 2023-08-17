ec22c3cfe0abf6287fe0d98d362020184e618ada
unknown <unknown>
2023-08-17 17:41:48 +0000

    llvm-nm ignore the Import symbol file for the --export-symbol option.
    Automated commit created by applying diff 550828
    
    Phabricator-ID: PHID-HMBT-vadyyulusnlmrbqxbqtk
    Review-ID: D158004

--- llvm/tools/llvm-nm/llvm-nm.cpp
+++ llvm/tools/llvm-nm/llvm-nm.cpp
@@ -2262,6 +2262,13 @@ static std::vector<NMSymbol> dumpSymbolNamesFromFile(StringRef Filename) {
   if (error(BufferOrErr.getError(), Filename))
     return SymbolList;
 
+  // Ignore parsing an imported symbol file that begins with #! when we export
+  // symbols.
+  const char *BuffStart = (*BufferOrErr)->getBufferStart();
+  if (ExportSymbols && (*BufferOrErr)->getBufferSize() >= 2 &&
+      BuffStart[0] == '#' && BuffStart[1] == '!')
+    return SymbolList;
+
   LLVMContext Context;
   LLVMContext *ContextPtr = NoLLVMBitcode ? nullptr : &Context;
   Expected<std::unique_ptr<Binary>> BinaryOrErr =
