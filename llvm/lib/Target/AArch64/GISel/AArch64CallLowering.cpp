536e074410c33c06da2d90e2f8cfa42e9e21a1ae
Martin Storsjö <martin@martin.st>
2023-08-18 12:53:13 +0000

    [AArch64] [GlobalISel] Fix clobbered callee saved registers with win64 varargs
    Automated commit created by applying diff 551479
    
    Phabricator-ID: PHID-HMBT-m3s557mjn5byadyah2gj
    Review-ID: D158272

--- llvm/lib/Target/AArch64/GISel/AArch64CallLowering.cpp
+++ llvm/lib/Target/AArch64/GISel/AArch64CallLowering.cpp
@@ -564,6 +564,10 @@ void AArch64CallLowering::saveVarArgRegisters(
     if (IsWin64CC) {
       GPRIdx = MFI.CreateFixedObject(GPRSaveSize,
                                      -static_cast<int>(GPRSaveSize), false);
+      if (GPRSaveSize & 15)
+        // The extra size here, if triggered, will always be 8.
+        MFI.CreateFixedObject(16 - (GPRSaveSize & 15),
+                              -(int)alignTo(GPRSaveSize, 16), false);
     } else
       GPRIdx = MFI.CreateStackObject(GPRSaveSize, Align(8), false);
 
