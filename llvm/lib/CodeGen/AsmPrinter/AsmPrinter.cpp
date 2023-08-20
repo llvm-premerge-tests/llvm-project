a24f357f82b46258a39ba6e511216876dc09a3c8
Rahman Lavaee <rahmanl@google.com>
2023-08-20 18:36:53 +0000

    Refactor profile reading.
    Automated commit created by applying diff 551864
    
    Phabricator-ID: PHID-HMBT-6ps2tz24nycyotrdmia6
    Review-ID: D158374

--- llvm/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
+++ llvm/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
@@ -426,6 +426,7 @@ void AsmPrinter::getAnalysisUsage(AnalysisUsage &AU) const {
   AU.addRequired<MachineOptimizationRemarkEmitterPass>();
   AU.addRequired<GCModuleInfo>();
   AU.addRequired<LazyMachineBlockFrequencyInfoPass>();
+  AU.addRequired<MachineLoopInfo>();
 }
 
 bool AsmPrinter::doInitialization(Module &M) {
@@ -1345,6 +1346,10 @@ static uint32_t getBBAddrMapMetadata(const MachineBasicBlock &MBB) {
       .encode();
 }
 
+ // auto LoopInfo = MLI->getLoopFor(&MBB);
+  // if (LoopInfo)
+   // IsLoopHeader = LoopInfo->getHeader() == &MBB;
+
 void AsmPrinter::emitBBAddrMapSection(const MachineFunction &MF) {
   MCSection *BBAddrMapSection =
       getObjFileLowering().getBBAddrMapSection(*MF.getSection());
@@ -1381,6 +1386,12 @@ void AsmPrinter::emitBBAddrMapSection(const MachineFunction &MF) {
     // always be computed from their offsets.
     emitLabelDifferenceAsULEB128(MBB.getEndSymbol(), MBBSymbol);
     // Emit the Metadata.
+    //MLI = getAnalysisIfAvailable<MachineLoopInfo>();
+    //if (!MLI) {
+    //  OwnedMLI = std::make_unique<MachineLoopInfo>();
+    //  OwnedMLI->getBase().analyze(MDT->getBase());
+    //  MLI = OwnedMLI.get();
+    //}
     OutStreamer->emitULEB128IntValue(getBBAddrMapMetadata(MBB));
     PrevMBBEndSymbol = MBB.getEndSymbol();
   }
