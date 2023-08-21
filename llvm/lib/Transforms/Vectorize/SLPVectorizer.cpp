5b0e26766f913e683c1439d4e856a1b8f1b3e186
Mel Chen <mel.chen@sifive.com>
2023-08-21 08:53:16 +0000

    [LoopVectorize] Vectorize select-cmp reduction pattern for increasing integer induction variable
    Automated commit created by applying diff 551932
    
    Phabricator-ID: PHID-HMBT-jxvmdcvf3byrhs33aqow
    Review-ID: D150851

--- llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
+++ llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -14137,6 +14137,8 @@ private:
     case RecurKind::FMulAdd:
     case RecurKind::IAnyOf:
     case RecurKind::FAnyOf:
+    case RecurKind::IFindLastIV:
+    case RecurKind::FFindLastIV:
     case RecurKind::None:
       llvm_unreachable("Unexpected reduction kind for repeated scalar.");
     }
@@ -14226,6 +14228,8 @@ private:
     case RecurKind::FMulAdd:
     case RecurKind::IAnyOf:
     case RecurKind::FAnyOf:
+    case RecurKind::IFindLastIV:
+    case RecurKind::FFindLastIV:
     case RecurKind::None:
       llvm_unreachable("Unexpected reduction kind for reused scalars.");
     }
