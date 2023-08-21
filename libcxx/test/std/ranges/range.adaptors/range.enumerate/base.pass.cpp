46e49e29d8ab23d62feb6efdd47887ce5d2ac65b
Hristo Hristov <zingam@outlook.com>
2023-08-21 07:57:11 +0000

    [libc++][ranges] P2116R9: Implements `views::enumerate`
    Automated commit created by applying diff 551923
    
    Phabricator-ID: PHID-HMBT-y67dt7yl2u2ksse4tphi
    Review-ID: D157193

---
+++ libcxx/test/std/ranges/range.adaptors/range.enumerate/base.pass.cpp
@@ -0,0 +1,57 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
+
+// <ranges>
+
+// class enumerate_view
+
+// constexpr V base() const & requires copy_constructible<V> { return base_; }
+// constexpr V base() && { return std::move(base_); }
+
+#include <cassert>
+#include <ranges>
+
+#include "types.h"
+
+constexpr bool test() {
+  // Check the const& overload
+  {
+    int buff[] = {0, 1, 2, 3};
+
+    Range const range(buff, buff + 4);
+
+    std::ranges::enumerate_view<Range> const view{range};
+    std::same_as<Range> decltype(auto) result = view.base();
+    assert(result.wasCopyInitialized);
+    assert(range.begin() == result.begin());
+    assert(range.end() == result.end());
+  }
+  // Check the && overload
+  {
+    int buff[] = {0, 1, 2, 3};
+
+    Range const range(buff, buff + 4);
+
+    std::ranges::enumerate_view<Range> view{range};
+    std::same_as<Range> decltype(auto) result = std::move(view).base();
+    assert(result.wasMoveInitialized);
+    assert(range.begin() == result.begin());
+    assert(range.end() == result.end());
+  }
+
+  return true;
+}
+
+int main(int, char**) {
+  test();
+  static_assert(test());
+
+  return 0;
+}
