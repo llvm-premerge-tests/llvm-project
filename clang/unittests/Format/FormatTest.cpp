fd0e1c6a3ce7c8026c382782034e2c7c5426d77b
unknown <unknown>
2023-08-22 11:21:25 +0000

    [clang-format] Fix weird handling of AfterColon
    Automated commit created by applying diff 552310
    
    Phabricator-ID: PHID-HMBT-jykncwdnotfydcvfksx7
    Review-ID: D158505

--- clang/unittests/Format/FormatTest.cpp
+++ clang/unittests/Format/FormatTest.cpp
@@ -7950,6 +7950,11 @@ TEST_F(FormatTest, BreakConstructorInitializersAfterColon) {
   verifyFormat("template <typename T>\n"
                "Constructor() : Initializer(FitsOnTheLine) {}",
                getStyleWithColumns(Style, 50));
+  verifyFormat(
+      "Class::Class(int some, int arguments, int loooooooooooooonoooooog,\n"
+      "             int mooooooooooooore) noexcept :\n"
+      "    Super{some, arguments}, Member{5}, Member2{2} {}",
+      Style);
   Style.PackConstructorInitializers = FormatStyle::PCIS_NextLine;
   verifyFormat(
       "SomeClass::Constructor() :\n"
@@ -7987,8 +7992,8 @@ TEST_F(FormatTest, BreakConstructorInitializersAfterColon) {
       "    aaaaaaaaaaaaaaa(aaaaaaaaaaaa) {}",
       Style);
   verifyFormat("Constructor(aaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa,\n"
-               "            aaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa) :\n"
-               "    aaaaaaaaaa(aaaaaa) {}",
+               "            aaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa) : "
+               "aaaaaaaaaa(aaaaaa) {}",
                Style);
 
   verifyFormat("Constructor() :\n"
