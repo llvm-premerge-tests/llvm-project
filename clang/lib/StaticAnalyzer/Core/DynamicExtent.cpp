70410bf6e48f24eb97a0c977f62e57c752e924e7
unknown <unknown>
2023-08-22 10:05:40 +0000

    [analyzer] Compute FAM dynamic size
    Automated commit created by applying diff 552286
    
    Phabricator-ID: PHID-HMBT-flofqulmxbaqlhconr36
    Review-ID: D158499

--- clang/lib/StaticAnalyzer/Core/DynamicExtent.cpp
+++ clang/lib/StaticAnalyzer/Core/DynamicExtent.cpp
@@ -26,11 +26,59 @@ namespace clang {
 namespace ento {
 
 DefinedOrUnknownSVal getDynamicExtent(ProgramStateRef State,
+                                      const MemRegion *MR, SValBuilder &SVB);
+
+static bool isFlexibleArrayMember(const FieldDecl *FD) {
+  const auto *RD = FD->getParent();
+  if (!RD->hasFlexibleArrayMember())
+    return false;
+
+  const FieldDecl *LastFD = nullptr;
+  for (const FieldDecl *It : RD->fields())
+    LastFD = It;
+
+  return FD == LastFD;
+}
+
+static std::optional<DefinedOrUnknownSVal>
+getFlexibleArrayExtent(ProgramStateRef State, const MemRegion *MR,
+                       SValBuilder &SVB) {
+  const auto *FieldMR = dyn_cast<FieldRegion>(MR);
+  if (nullptr == FieldMR)
+    return std::nullopt;
+
+  const FieldDecl *FD = FieldMR->getDecl();
+  if (!isFlexibleArrayMember(FD))
+    return std::nullopt;
+
+  const RecordDecl *RD = FD->getParent();
+  const MemRegion *BaseMR = FieldMR->getBaseRegion();
+  auto BaseSize = getDynamicExtent(State, BaseMR, SVB);
+
+  auto &C = SVB.getContext();
+  uint64_t RecordSize = C.getTypeSizeInChars(C.getRecordType(RD)).getQuantity();
+  SVal RecordSizeVal = SVB.makeIntVal(RecordSize, C.getSizeType());
+
+  SVal BaseTooSmall =
+      SVB.evalBinOp(State, BO_LT, BaseSize, RecordSizeVal, C.BoolTy);
+  if (!BaseTooSmall.isUndef() &&
+      State->assume(*BaseTooSmall.getAs<DefinedOrUnknownSVal>(), true))
+    return std::nullopt;
+
+  SVal FlexibleArrayExtent =
+      SVB.evalBinOp(State, BO_Sub, BaseSize, RecordSizeVal, C.getSizeType());
+  return FlexibleArrayExtent.getAs<DefinedOrUnknownSVal>();
+}
+
+DefinedOrUnknownSVal getDynamicExtent(ProgramStateRef State,
                                       const MemRegion *MR, SValBuilder &SVB) {
   MR = MR->StripCasts();
 
   if (const DefinedOrUnknownSVal *Size = State->get<DynamicExtentMap>(MR))
     return *Size;
+
+  if (auto FlexibleArrayExtent = getFlexibleArrayExtent(State, MR, SVB))
+    return *FlexibleArrayExtent;
 
   return MR->getMemRegionManager().getStaticSize(MR, SVB);
 }
