fe82dc793a363fabe044ff915015271c59a745f2
Daniel Cheng <zetafunction@gmail.com>
2023-08-26 15:37:08 +0000

    [libc++] Implement LWG3545: std::pointer_traits should be SFINAE-friendly.
    Automated commit created by applying diff 553737
    
    Phabricator-ID: PHID-HMBT-tzdosd4b2i2max3mzvng
    Review-ID: D158922

---
+++ libcxx/test/std/utilities/memory/pointer.conversion/to_address_without_pointer_traits.pass.cpp
@@ -0,0 +1,48 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// <memory>
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17
+
+// template <class Ptr> constexpr auto to_address(const Ptr& p) noexcept;
+//     Should not require a specialization of pointer_traits for Ptr.
+
+#include <memory>
+
+struct IntPtr {
+  constexpr int* operator->() const { return ptr; }
+
+  int* ptr;
+};
+
+template <typename T, bool>
+struct TemplatedPtr {
+  constexpr T* operator->() const { return ptr; }
+
+  T* ptr;
+};
+
+constexpr bool test() {
+  int i = 0;
+
+  static_assert(std::to_address(IntPtr{nullptr}) == nullptr);
+  static_assert(std::to_address(IntPtr{&i}) == &i);
+
+  bool b = false;
+
+  static_assert(std::to_address(TemplatedPtr<bool, true>{nullptr}) == nullptr);
+  static_assert(std::to_address(TemplatedPtr<bool, true>{&b}) == &b);
+
+  return true;
+}
+
+int main(int, char**) {
+  static_assert(test());
+  return 0;
+}
