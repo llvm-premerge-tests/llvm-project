8e8bca1dcfa5dd944cd65e992ab123938f1acc63
Victor Kingi <victor.kingi@arm.com>
2023-08-29 14:11:52 +0000

    [Flang][Driver] Add regex support for R_Group options
    Automated commit created by applying diff 554316
    
    Phabricator-ID: PHID-HMBT-grfksmjcebduyktvvtzs
    Review-ID: D158436

--- flang/test/Driver/optimization-remark.f90
+++ flang/test/Driver/optimization-remark.f90
@@ -11,37 +11,69 @@
 ! RUN: %flang %s -O1 -Rno-pass-missed -c 2>&1 | FileCheck %s --allow-empty --check-prefix=NO-REMARKS
 ! RUN: %flang %s -O1 -Rno-pass-analysis -c 2>&1 | FileCheck %s --allow-empty --check-prefix=NO-REMARKS
 
+! Check valid -Rpass regex
+! RUN: %flang %s -O1 -Rpass=loop -c 2>&1 | FileCheck %s --check-prefix=PASS-REGEX
+
+! Check valid -Rpass-missed regex
+! RUN: %flang %s -O1 -Rpass-missed=loop -c 2>&1 | FileCheck %s --check-prefix=MISSED-REGEX
+
+! Check valid -Rpass-analysis regex
+! RUN: %flang %s -O1 -Rpass-analysis=loop -c 2>&1 | FileCheck %s --check-prefix=ANALYSIS-REGEX
+
 ! Check full -Rpass message is emitted
-! RUN: %flang %s -O1 -Rpass -c 2>&1 | FileCheck %s
+! RUN: %flang %s -O1 -Rpass -c 2>&1 | FileCheck %s --check-prefix=PASS
 
 ! Check full -Rpass-missed message is emitted
-! RUN: %flang %s -O1 -Rpass-missed -c 2>&1 | FileCheck %s --check-prefix=REMARKS-MISSED
+! RUN: %flang %s -O1 -Rpass-missed -c 2>&1 | FileCheck %s --check-prefix=MISSED
 
 ! Check full -Rpass-analysis message is emitted
-! RUN: %flang %s -O1 -Rpass-analysis -c 2>&1 | FileCheck %s --check-prefix=REMARKS-ANALYSIS
+! RUN: %flang %s -O1 -Rpass-analysis -c 2>&1 | FileCheck %s --check-prefix=ANALYSIS
 
-! CHECK: remark: Loop deleted because it is invariant
-! REMARKS-MISSED: {{.*}} will not be inlined into {{.*}} because its definition is unavailable
-! REMARKS-MISSED: remark: loop not vectorized
-! REMARKS-MISSED-NOT: loop not vectorized: instruction cannot be vectorized
-! REMARKS-ANALYSIS: remark: loop not vectorized: instruction cannot be vectorized
-! REMARKS-ANALYSIS-NOT: {{.*}} will not be inlined into {{.*}} because its definition is unavailable
+
+! PASS:               remark: hoisting load
+! PASS:               remark: Loop deleted because it is invariant
+
+! PASS-REGEX-NOT:     remark: hoisting load
+! PASS-REGEX:         remark: Loop deleted because it is invariant
+
+! MISSED:             remark: failed to hoist load with loop-invariant address because load is conditionally executed
+! MISSED:             remark: loop not vectorized
+! MISSED-NOT:         remark: loop not vectorized: unsafe dependent memory operations in loop. Use #pragma loop distribute(enable) to allow loop distribution to attempt to isolate the offending operations into a separate loop
+!                     Unknown data dependence.
+
+! MISSED-REGEX-NOT:   remark: failed to hoist load with loop-invariant address because load is conditionally executed
+! MISSED-REGEX:       remark: loop not vectorized
+
+! ANALYSIS-REGEX:     remark: loop not vectorized: unsafe dependent memory operations in loop. Use #pragma loop distribute(enable) to allow loop distribution to attempt to isolate the offending operations into a separate loop
+!                     Unknown data dependence.
+! ANALYSIS-REGEX-NOT: remark: {{.*}}: IR instruction count changed from {{[0-9]+}} to {{[0-9]+}}; Delta: {{[0-9]+}}
+
+! ANALYSIS:           remark: loop not vectorized: unsafe dependent memory operations in loop. Use #pragma loop distribute(enable) to allow loop distribution to attempt to isolate the offending operations into a separate loop
+!                     Unknown data dependence.
+! ANALYSIS:           remark: {{.*}}: IR instruction count changed from {{[0-9]+}} to {{[0-9]+}}; Delta: {{[0-9]+}}
+! ANALYSIS-NOT:       remark: failed to hoist load with loop-invariant address because load is conditionally executed
 
 ! REMARKS: remark:
 ! NO-REMARKS-NOT: remark:
 
-program forttest
-    implicit none
-    real, dimension(1:50) :: aR1
-    integer :: n
+subroutine swap_real(a1, a2)
+   implicit none
 
-    do n = 1,50
+   real, dimension(1:2) :: aR1
+   integer :: i, n
+   real, intent(inout) :: a1(:), a2(:)
+   real :: a
+
+!  Swap
+   do i = 1, min(size(a1), size(a2))
+      a = a1(i)
+      a1(i) = a2(i)
+      a2(i) = a
+   enddo
+
+! Do a random loop to generate a successful loop-delete pass
+    do n = 1,2
         aR1(n) = n * 1.34
-        print *, "hello"
     end do
 
-    do n = 1,50
-        aR1(n) = n * 1.34
-    end do
-
-end program forttest
+end subroutine swap_real
