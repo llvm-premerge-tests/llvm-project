1317f468f5d84353c16ee749ffd7126fc09e1b6e
Ian Anderson <iana@apple.com>
2023-08-30 21:18:43 +0000

    [Modules] Make clang modules for the C standard library headers
    Automated commit created by applying diff 554832
    
    Phabricator-ID: PHID-HMBT-5zndvr52ylwstkt2izek
    Review-ID: D159064

--- clang/test/Modules/compiler_builtins.m
+++ clang/test/Modules/compiler_builtins.m
@@ -11,3 +11,17 @@
 #ifdef __AVX2__
 @import _Builtin_intrinsics.intel.avx2;
 #endif
+
+@import _Builtin_float;
+@import _Builtin_inttypes;
+@import _Builtin_iso646;
+@import _Builtin_limits;
+@import _Builtin_stdalign;
+@import _Builtin_stdarg;
+@import _Builtin_stdatomic;
+@import _Builtin_stdbool;
+@import _Builtin_stddef;
+@import _Builtin_stdint;
+@import _Builtin_stdnoreturn;
+@import _Builtin_tgmath;
+@import _Builtin_unwind;
