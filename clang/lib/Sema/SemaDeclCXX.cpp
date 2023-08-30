ef4aead3e94d3f8c810b7d765c7bebb85abe03e1
unknown <unknown>
2023-08-30 08:46:33 +0000

    Improve error message for constexpr constructors of virtual base classes
    Automated commit created by applying diff 554629
    
    Phabricator-ID: PHID-HMBT-mls256getr73gyqqqjok
    Review-ID: D158540

--- clang/lib/Sema/SemaDeclCXX.cpp
+++ clang/lib/Sema/SemaDeclCXX.cpp
@@ -7814,6 +7814,10 @@ bool Sema::CheckExplicitlyDefaultedSpecialMember(CXXMethodDecl *MD,
                                 ? diag::err_incorrect_defaulted_consteval
                                 : diag::err_incorrect_defaulted_constexpr)
         << CSM;
+    if (!MD->isConsteval()) {
+      Diag(MD->getBeginLoc(), diag::note_incorrect_defaulted_constexpr)
+          << RD << MD;
+    }
     // FIXME: Explain why the special member can't be constexpr.
     HadError = true;
   }
