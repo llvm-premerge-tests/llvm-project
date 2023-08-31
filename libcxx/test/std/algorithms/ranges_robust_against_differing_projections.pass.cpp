8950532261dcd6afee54eac042f62b1377121e43
zijunzhao <zijunzhao@google.com>
2023-08-31 01:20:23 +0000

    [libc++] Implement ranges::ends_with
    Automated commit created by applying diff 554883
    
    Phabricator-ID: PHID-HMBT-fq53jhne2iegxhry3f4k
    Review-ID: D150831

--- libcxx/test/std/algorithms/ranges_robust_against_differing_projections.pass.cpp
+++ libcxx/test/std/algorithms/ranges_robust_against_differing_projections.pass.cpp
@@ -54,6 +54,7 @@ constexpr bool test_all() {
   auto proj1 = [](int x) { return x * -1; };
   auto proj2 = [](A a) { return a.x * -1; };
 
+  test(std::ranges::ends_with, in, in2, eq, proj1, proj2);
   test(std::ranges::equal, in, in2, eq, proj1, proj2);
   test(std::ranges::lexicographical_compare, in, in2, eq, proj1, proj2);
   test(std::ranges::is_permutation, in, in2, eq, proj1, proj2);
@@ -77,7 +78,7 @@ constexpr bool test_all() {
   test(std::ranges::set_union, in, in2, out2, less, proj1, proj2);
 #if TEST_STD_VER > 20
   test(std::ranges::starts_with, in, in2, eq, proj1, proj2);
-  // test(std::ranges::ends_with, in, in2, eq, proj1, proj2);
+  test(std::ranges::ends_with, in, in2, eq, proj1, proj2);
 #endif
 
   return true;
