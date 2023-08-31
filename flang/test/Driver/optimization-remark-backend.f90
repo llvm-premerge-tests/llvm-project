4453920d9e3c1f8ff61e57041fb10707fa9f54db
Victor Kingi <victor.kingi@arm.com>
2023-08-31 09:20:14 +0000

    [Flang][Driver] Add support for backend passes in R_Group
    Automated commit created by applying diff 554944
    
    Phabricator-ID: PHID-HMBT-3fijxpve63dxatbpe25g
    Review-ID: D159260

---
+++ flang/test/Driver/optimization-remark-backend.f90
@@ -0,0 +1,21 @@
+! This file tests backend passes emitted by the -Rpass family of flags
+! loop-delete isn't enabled at O0 so we use at least O1
+
+! DEFINE: %{output} = -S -o /dev/null 2>&1
+
+! RUN: %flang %s -O1 -Rpass-missed %{output} 2>&1 | FileCheck %s --check-prefix=MISSED
+! RUN: %flang %s -O1 -Rpass-analysis %{output} 2>&1 | FileCheck %s --check-prefix=ANALYSIS
+
+! MISSED:   {{[0-9]+}} virtual registers copies {{.*}} total copies cost generated in function [-Rpass-missed=regalloc]
+! ANALYSIS: remark: BasicBlock:
+! ANALYSIS: [-Rpass-analysis=asm-printer]
+
+program forttest
+    implicit none
+    integer :: n
+
+    do n = 1,2
+        print *, ""
+    end do
+
+end program forttest
