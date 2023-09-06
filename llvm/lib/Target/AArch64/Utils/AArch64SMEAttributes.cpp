2722fa7a5a1f41c438f25a40fb41db8c6cf23c5c
unknown <unknown>
2023-09-06 19:06:53 +0000

    [AArch64][SME] Enable TPIDR2 lazy-save for za_preserved
    Automated commit created by applying diff 556065
    
    Phabricator-ID: PHID-HMBT-u5wsqmhr3v2hlsr3wvia
    Review-ID: D159186

--- llvm/lib/Target/AArch64/Utils/AArch64SMEAttributes.cpp
+++ llvm/lib/Target/AArch64/Utils/AArch64SMEAttributes.cpp
@@ -28,8 +28,18 @@ void SMEAttrs::set(unsigned M, bool Enable) {
 
 SMEAttrs::SMEAttrs(const CallBase &CB) {
   *this = SMEAttrs(CB.getAttributes());
-  if (auto *F = CB.getCalledFunction())
-    set(SMEAttrs(*F).Bitmask);
+  if (auto *F = CB.getCalledFunction()) {
+    set(SMEAttrs(*F).Bitmask | SMEAttrs(F->getName()).Bitmask);
+  }
+}
+
+SMEAttrs::SMEAttrs(StringRef FuncName) : Bitmask(0) {
+  if (FuncName == "__arm_tpidr2_save" || FuncName == "__arm_sme_state")
+    Bitmask |= (SMEAttrs::SM_Compatible | SMEAttrs::ZA_Preserved |
+                SMEAttrs::ZA_NoLazySave);
+  if (FuncName == "__arm_tpidr2_restore")
+    Bitmask |= (SMEAttrs::SM_Compatible | SMEAttrs::ZA_Shared |
+                SMEAttrs::ZA_NoLazySave);
 }
 
 SMEAttrs::SMEAttrs(const AttributeList &Attrs) {
