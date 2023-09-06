7546e317daeb91638679e3e56768a3077d6f4a7c
unknown <unknown>
2023-09-06 14:38:36 +0000

    Do not generate error about unsupported target specific  options when using -### or -v
    Automated commit created by applying diff 556033
    
    Phabricator-ID: PHID-HMBT-zly2bu5zgikhvtjlwl5j
    Review-ID: D159361

---
+++ clang/test/Driver/suppress-err-on-target-specific-options.c
@@ -0,0 +1,23 @@
+// RUN: %clang --target=aarch64-none-gnu --verbose -mcpu= 2>&1 | FileCheck %s --check-prefix=WARNING
+// RUN: %clang --target=aarch64-none-gnu --verbose -march= 2>&1 | FileCheck %s --check-prefix=WARNING
+// RUN: %clang --target=aarch64-none-gnu -### -mcpu= 2>&1 | FileCheck %s --check-prefix=WARNING
+// RUN: %clang --target=aarch64-none-gnu -### -march= 2>&1 | FileCheck %s --check-prefix=WARNING
+
+// RUN: %clang --target=x86_64-unknown-linux-gnu --verbose -mcpu= 2>&1 | FileCheck %s --check-prefix=WARNING
+// RUN: %clang --target=x86_64-unknown-linux-gnu --verbose -march= 2>&1 | FileCheck %s --check-prefix=WARNING
+// RUN: %clang --target=x86_64-unknown-linux-gnu -### -mcpu= 2>&1 | FileCheck %s --check-prefix=WARNING
+// RUN: %clang --target=x86_64-unknown-linux-gnu -### -march= 2>&1 | FileCheck %s --check-prefix=WARNING
+
+// RUN: touch %t.c
+// RUN: %if x86-registered-target %{ not %clang --target=x86_64-unknown-linux-gnu --verbose -mcpu=corei7 %t.c -S 2>&1 | FileCheck %s --check-prefix=ERROR-X86 %}
+// RUN: %if x86-registered-target %{ not %clang --target=x86_64-unknown-linux-gnu -### -mcpu=corei7 %t.c -S 2>&1 | FileCheck %s --check-prefix=ERROR-X86 %}
+
+// RUN: %if aarch64-registered-target %{ %clang --target=aarch64-none-gnu --verbose -mcpu=neoverse-v1 %t.c -S 2>&1 | FileCheck %s --check-prefix=ERROR-AARCH64 %}
+// RUN: %if aarch64-registered-target %{ %clang --target=aarch64-none-gnu -### -mcpu=neoverse-v1 %t.c 2>&1 -S | FileCheck %s --check-prefix=ERROR-AARCH64 %}
+
+// In situation when there is no compilation/linking clang should not emit error
+// about target specific options, but just warn that are not used.
+WARNING: warning: argument unused during compilation
+
+ERROR-X86: error: unsupported option {{.*}} for target 'x86_64-unknown-linux-gnu'
+ERROR-AARCH64-NOT: error: unsupported option {{.*}} for target 'aarch64-none-gnu'
