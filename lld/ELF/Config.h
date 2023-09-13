22752e157c3e9f6ade9e069ef29c83e2d8df365f
spupyrev <spupyrev@fb.com>
2023-09-13 21:14:49 +0000

    [ELF] A new code layout algorithm for function reordering [3a/3]
    Automated commit created by applying diff 556722
    
    Phabricator-ID: PHID-HMBT-fsjpzlyqfljix6fu7o7m
    Review-ID: D152840

--- lld/ELF/Config.h
+++ lld/ELF/Config.h
@@ -289,6 +289,7 @@ struct Config {
   bool undefinedVersion;
   bool unique;
   bool useAndroidRelrTags = false;
+  bool useCDSort;
   bool warnBackrefs;
   llvm::SmallVector<llvm::GlobPattern, 0> warnBackrefsExclude;
   bool warnCommon;
