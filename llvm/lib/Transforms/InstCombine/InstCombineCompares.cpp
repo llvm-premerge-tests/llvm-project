018fada09dd49ceb5b35aa017dc7e212665eafb0
Noah Goldstein <goldstein.w.n@gmail.com>
2023-09-13 20:01:41 +0000

    [InstCombine] Folding multiuse `(icmp eq/ne (or X, Y), Y)` for 2 uses of `Y`
    Automated commit created by applying diff 556709
    
    Phabricator-ID: PHID-HMBT-pqegowksd76n7mwhled6
    Review-ID: D159062

--- llvm/lib/Transforms/InstCombine/InstCombineCompares.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineCompares.cpp
@@ -4663,7 +4663,7 @@ static Instruction *foldICmpOrXX(ICmpInst &I, const SimplifyQuery &Q,
 
   if (ICmpInst::isEquality(Pred) && Op0->hasOneUse()) {
     // icmp (X | Y) eq/ne Y --> (X & ~Y) eq/ne 0 if Y is freely invertible
-    if (IC.isFreeToInvert(Op1, Op1->hasOneUse()))
+    if (IC.isFreeToInvert(Op1, !Op1->hasNUsesOrMore(3)))
       return new ICmpInst(Pred,
                           IC.Builder.CreateAnd(A, IC.Builder.CreateNot(Op1)),
                           Constant::getNullValue(Op1->getType()));
