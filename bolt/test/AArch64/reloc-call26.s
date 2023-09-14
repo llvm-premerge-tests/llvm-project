e5b6837b964e0fe60559b95bf5e27b2a4c573654
unknown <unknown>
2023-09-14 11:31:44 +0000

    [Bolt] fix a relocation bug for R_AARCH64_CALL26
    Automated commit created by applying diff 556772
    
    Phabricator-ID: PHID-HMBT-mkt5ezw5ki75qlirdt4f
    Review-ID: D159513

--- bolt/test/AArch64/reloc-call26.s
+++ bolt/test/AArch64/reloc-call26.s
@@ -6,24 +6,30 @@
 # RUN: llvm-mc -filetype=obj -triple aarch64-unknown-unknown \
 # RUN:   %s -o %t.o
 # RUN: %clang %cflags %t.o -o %t.exe -Wl,-q
-# RUN: llvm-bolt %t.exe -o %t.bolt --funcs=func1
+# RUN: llvm-bolt %t.exe -o %t.bolt --funcs=func1,func2 --force-patch
 # RUN: llvm-objdump -d --disassemble-symbols='_start' %t.bolt | \
 # RUN:   FileCheck %s
 
 # CHECK: {{.*}} bl {{.*}} <func1>
+# CHECK: {{.*}} bl {{.*}} <func2>
 
   .text
   .align 4
-  .global _start
-  .type _start, %function
-_start:
-  bl func1
-  mov     w8, #93
-  svc     #0
-  .size _start, .-_start
-
   .global func1
   .type func1, %function
 func1:
   ret
   .size func1, .-func1
+  .global _start
+  .type _start, %function
+_start:
+  bl func1
+  bl func2
+  mov     w8, #93
+  svc     #0
+  .size _start, .-_start
+  .global func2
+  .type func2, %function
+func2:
+  ret
+  .size func2, .-func2
