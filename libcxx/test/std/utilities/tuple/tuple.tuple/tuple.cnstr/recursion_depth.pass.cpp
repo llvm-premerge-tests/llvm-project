287b4bbd1cd85fd6c1885fc0d61ed21f2fbff3e3
Louis Dionne <ldionne.2@gmail.com>
2023-09-19 19:12:58 +0000

    [libc++] Use _Lazy in tuple constructors
    Automated commit created by applying diff 557068
    
    Phabricator-ID: PHID-HMBT-o7ui42mpuqr7z6ougqlo
    Review-ID: D132509

--- libcxx/test/std/utilities/tuple/tuple.tuple/tuple.cnstr/recursion_depth.pass.cpp
+++ libcxx/test/std/utilities/tuple/tuple.tuple/tuple.cnstr/recursion_depth.pass.cpp
@@ -15,11 +15,31 @@
 #include <cassert>
 #include <utility>
 
+#include "test_macros.h"
+
+template <std::size_t I>
+struct constructible_from_integral_constant : std::integral_constant<std::size_t, I> { };
+
 template <std::size_t... I>
 constexpr void CreateTuple(std::index_sequence<I...>) {
-  std::tuple<decltype(I)...> tuple(I...);
-  assert(std::get<0>(tuple) == 0);
-  assert(std::get<sizeof...(I)-1>(tuple) == sizeof...(I)-1);
+  using LargeTuple = std::tuple<std::integral_constant<std::size_t, I>...>;
+  using TargetTuple = std::tuple<decltype(I)...>;
+  LargeTuple tuple(std::integral_constant<std::size_t, I>{}...);
+  assert(std::get<0>(tuple).value == 0);
+  assert(std::get<sizeof...(I)-1>(tuple).value == sizeof...(I)-1);
+
+  TargetTuple t1 = tuple;                                  // converting copy constructor
+  TargetTuple t2 = std::move(tuple);                       // converting move constructor
+  TargetTuple t3 = static_cast<LargeTuple const&&>(tuple); // converting const rvalue constructor
+  TargetTuple t4;                                          // default constructor
+  (void)t1; (void)t2; (void)t3; (void)t4;
+
+#if TEST_STD_VER >= 20
+  t3 = tuple;                                              // converting copy assignment
+  t2 = std::move(tuple);                                   // converting move assignment
+  swap(t1, t4);                                            // swap
+#endif
+  // t1 == tuple;                                          // comparison does not work yet (we blow the constexpr stack)
 }
 
 constexpr bool test() {
