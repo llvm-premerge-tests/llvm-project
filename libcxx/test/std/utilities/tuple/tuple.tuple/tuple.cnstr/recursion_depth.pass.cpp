4b8a85a02d4f0ef59ed07aa435672a5ca6f2ee72
Louis Dionne <ldionne.2@gmail.com>
2023-09-19 21:14:45 +0000

    [libc++] Use _Lazy in tuple constructors
    Automated commit created by applying diff 557076
    
    Phabricator-ID: PHID-HMBT-mv4qvv4rlu6z4ynz5jet
    Review-ID: D132509

--- libcxx/test/std/utilities/tuple/tuple.tuple/tuple.cnstr/recursion_depth.pass.cpp
+++ libcxx/test/std/utilities/tuple/tuple.tuple/tuple.cnstr/recursion_depth.pass.cpp
@@ -15,11 +15,31 @@
 #include <cassert>
 #include <utility>
 
+#include "test_macros.h"
+
 template <std::size_t... I>
 constexpr void CreateTuple(std::index_sequence<I...>) {
-  std::tuple<decltype(I)...> tuple(I...);
-  assert(std::get<0>(tuple) == 0);
-  assert(std::get<sizeof...(I)-1>(tuple) == sizeof...(I)-1);
+  using LargeTuple = std::tuple<std::integral_constant<std::size_t, I>...>;
+  using TargetTuple = std::tuple<decltype(I)...>;
+  LargeTuple tuple(std::integral_constant<std::size_t, I>{}...);
+  assert(std::get<0>(tuple).value == 0);
+  assert(std::get<sizeof...(I)-1>(tuple).value == sizeof...(I)-1);
+
+  TargetTuple t1 = tuple;                                  // converting copy constructor from &
+  TargetTuple t2 = static_cast<LargeTuple const&>(tuple);  // converting copy constructor from const&
+  TargetTuple t3 = std::move(tuple);                       // converting rvalue constructor
+  TargetTuple t4 = static_cast<LargeTuple const&&>(tuple); // converting const rvalue constructor
+  TargetTuple t5;                                          // default constructor
+  (void)t1; (void)t2; (void)t3; (void)t4; (void)t5;
+
+#if TEST_STD_VER >= 20
+  t1 = tuple;                                              // converting assignment from &
+  t1 = static_cast<LargeTuple const&>(tuple);              // converting assignment from const&
+  t1 = std::move(tuple);                                   // converting assignment from &&
+  t1 = static_cast<LargeTuple const&&>(tuple);             // converting assignment from const&&
+  swap(t1, t2);                                            // swap
+#endif
+  // t1 == tuple;                                          // comparison does not work yet (we blow the constexpr stack)
 }
 
 constexpr bool test() {
