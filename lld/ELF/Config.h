17e4fb5058b7d473abcb3f8322ce6b445cc99d12
spupyrev <spupyrev@fb.com>
2023-09-25 17:39:05 +0000

    [ELF] A new code layout algorithm for function reordering [3a/3]
    Automated commit created by applying diff 557316
    
    Phabricator-ID: PHID-HMBT-lgcl26rfhvnebezfyv4b
    Review-ID: D152840

--- lld/ELF/Config.h
+++ lld/ELF/Config.h
@@ -60,7 +60,7 @@ enum class BsymbolicKind { None, NonWeakFunctions, Functions, NonWeak, All };
 enum class BuildIdKind { None, Fast, Md5, Sha1, Hexstring, Uuid };
 
 // For --call-graph-profile-sort={none,hfsort}.
-enum class CGProfileSortKind { None, Hfsort };
+enum class CGProfileSortKind { None, Hfsort, Cdsort };
 
 // For --discard-{all,locals,none}.
 enum class DiscardPolicy { Default, All, Locals, None };
@@ -293,6 +293,7 @@ struct Config {
   bool undefinedVersion;
   bool unique;
   bool useAndroidRelrTags = false;
+  bool useCacheDirectedSort;
   bool warnBackrefs;
   llvm::SmallVector<llvm::GlobPattern, 0> warnBackrefsExclude;
   bool warnCommon;
